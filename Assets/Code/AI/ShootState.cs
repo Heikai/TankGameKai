982d9ee66ccd1b61631774e1abd036d45118863b
Kai Heikkila <kai-kristian.heikkila@cs.tamk.fi>
2018-03-05 13:09:02 +0200

    Assignment 2

---
+++ Assets/Code/AI/ShootState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame.AI
+{
+    public class ShootState : AIStateBase
+    {
+
+        public float SqrShootingDistance
+        {
+            get { return Owner.ShootingDistance * Owner.ShootingDistance; }
+        }
+
+        public ShootState (EnemyUnit owner)
+            : base(owner, AIStateType.Shoot)
+        {
+            AddTransition(AIStateType.FollowTarget);
+            AddTransition(AIStateType.Patrol);
+        }
+
+        public override void Update()
+        {
+            Owner.Mover.Turn(Owner.Target.transform.position);
+            Owner.Weapon.Shoot();
+
+            Vector3 toPlayerVector = Owner.transform.position - Owner.Target.transform.position;
+            float sqrDistanceToPlayer = toPlayerVector.sqrMagnitude;
+
+            if (sqrDistanceToPlayer > SqrShootingDistance)
+            {
+                Owner.PerformTransition(AIStateType.FollowTarget);
+            }
+
+            if (Owner.Target.Health.CurrentHealth <= 0)
+            {
+                Owner.PerformTransition(AIStateType.Patrol);
+            }
+        }
+    }
+}
---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame
+{
+    public class CameraController : MonoBehaviour, ICameraFollow
+    {
+        [SerializeField]
+        private float _distance;
+        [SerializeField]
+        private float _angle;
+        [SerializeField]
+        private Transform _target;
+        private float _horizontalDistance;
+        private float _verticalDistance;
+        private float _angleInRad;
+
+        private void Update()
+        {
+            //Convert degrees to radians
+            _angleInRad = _angle * Mathf.Deg2Rad;
+
+            //Calculate correct x and z values for the camera
+            _horizontalDistance = Mathf.Sin(_angleInRad) * _distance;
+            _verticalDistance = Mathf.Cos(_angleInRad) * _distance;
+
+            //Move camera to the right position
+            transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y + _verticalDistance, _target.transform.position.z - _horizontalDistance);
+
+            //Rotate camera to the player
+            Vector3 relativePos = _target.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            transform.rotation = rotation;
+        }
+
+        public void SetAngle(float angle)
+        {
+            _angle = angle;
+            _angleInRad = _angle * Mathf.Rad2Deg;
+        }
+
+        public void SetDistance(float distance)
+        {
+            _distance = distance;
+        }
+
+        public void SetTarget(Transform targetTransform)
+        {
+            _target = targetTransform;
+        }
+    }
+}
